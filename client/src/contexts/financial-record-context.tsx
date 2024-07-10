bbb7c417fb1297462a85dc8bc656564fa8bf15a2
Rishabh-Sh1rma <130218510+Rishabh-Sh1rma@users.noreply.github.com>
2024-07-10 13:56:09 +0530

    Update financial-record-context.tsx

--- client/src/contexts/financial-record-context.tsx
+++ client/src/contexts/financial-record-context.tsx
@@ -33,7 +33,7 @@ export const FinancialRecordsProvider = ({
   const fetchRecords = async () => {
     if (!user) return;
     const response = await fetch(
-      `https://spend-organizer-final.onrender.com/getAllByUserID/${user.id}`, {
+      `https://spend-organizer-final.onrender.com/financial-records/getAllByUserID/${user.id}`, {
         method: "GET",
         
         headers: {
@@ -54,7 +54,7 @@ export const FinancialRecordsProvider = ({
   }, [user]);
 
   const addRecord = async (record: FinancialRecord) => {
-    const response = await fetch("https://spend-organizer-final.onrender.com", {
+    const response = await fetch("https://spend-organizer-final.onrender.com/financial-records", {
       method: "POST",
       body: JSON.stringify(record),
       headers: {
@@ -72,7 +72,7 @@ export const FinancialRecordsProvider = ({
 
   const updateRecord = async (id: string, newRecord: FinancialRecord) => {
     const response = await fetch(
-      `https://spend-organizer-final.onrender.com/${id}`,
+      `https://spend-organizer-final.onrender.com/financial-records/${id}`,
       {
         method: "PUT",
         body: JSON.stringify(newRecord),
@@ -100,7 +100,7 @@ export const FinancialRecordsProvider = ({
 
   const deleteRecord = async (id: string) => {
     const response = await fetch(
-      `https://spend-organizer-final.onrender.com/${id}`,
+      `https://spend-organizer-final.onrender.com/financial-records/${id}`,
       {
         method: "DELETE",
       }
