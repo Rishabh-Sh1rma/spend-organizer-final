74a9f0ba7117d2d7d289ba67a342cdd086176fe8
Rishabh-Sh1rma <rishabhar1974@gmail.com>
2024-07-10 13:10:17 +0530

    Initial commit with my information

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import financialRecordRouter from "./routes/financial-records";
+import cors from "cors";
+
+const app: Express = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
+
+const mongoURI: string =
+  //"mongodb+srv://machadop1407:UvOUTpZW7EH85MZl@personalfinancetracker.qb0edtk.mongodb.net/";
+  "mongodb+srv://rishabhar1974:1dW3949s7CqOVmJ4@personalfinancetracker.m2zfnkw.mongodb.net/";
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("CONNECTED TO MONGODB!"))
+  .catch((err) => console.error("Failed to Connect to MongoDB:", err));
+
+app.use("/financial-records", financialRecordRouter);
+
+app.listen(port, () => {
+  console.log(`Server Running on Port ${port}`);
+});
